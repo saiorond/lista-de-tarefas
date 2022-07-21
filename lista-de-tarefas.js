7419210e9f7e915e83dca32b25bfc99a3b6dd4ed
Débora <d.saioron@hotmail.com>
2022-07-21 20:38:24 -0300

    adicionando arquivos

---
+++ lista-de-tarefas.js
@@ -0,0 +1,15 @@
+//const date = new Date();
+
+//const ano = date.getFullYear();
+//const dia = date.getDate();
+//const mes = date.getMonth() + 1;
+
+//function formatarData(date, format) {
+//    formatDate(today, 'dd/mm/aa');
+
+//    format.replace('dd',getDate())
+//    format.replace('mm', date.getMonth() + 1)
+//    format.replace('aa', date.getFullYear())
+//}
+
+var dataEscolhida = document.getElementById('data_escolhida').value 
